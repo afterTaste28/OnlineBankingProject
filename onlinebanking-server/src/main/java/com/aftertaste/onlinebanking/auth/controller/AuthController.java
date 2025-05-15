9db9296a0ce15e32f5f516f04590df7226a15589
devendrakolhe <aftertaste28@gmail.com>
2025-05-15 09:29:07 +0530

    Removing isUp non required method

--- onlinebanking-server/src/main/java/com/aftertaste/onlinebanking/auth/controller/AuthController.java
+++ onlinebanking-server/src/main/java/com/aftertaste/onlinebanking/auth/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.aftertaste.onlinebanking.auth.dto.LoginRequest;
 import com.aftertaste.onlinebanking.auth.dto.RegisterRequest;
 import com.aftertaste.onlinebanking.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,11 +24,5 @@ public class AuthController {
     @PostMapping("/login")
     public void loginUser(@RequestBody LoginRequest loginRequestDTO){
         System.out.println("Inside login method:" +loginRequestDTO.toString());
-    }
-
-    @GetMapping("/isUp")
-    public void isUp(){
-
-        System.out.println("Up and runnign");
     }
 }
